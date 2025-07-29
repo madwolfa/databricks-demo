248fa3744cca80458d54d40ac4e7f80a36a3896c
Pavlo Vovk <pvovk@origamirisk.com>
2025-07-29 16:41:56 -0500

    chore: try github oidc

--- providers.tf
+++ providers.tf
@@ -1,6 +1,7 @@
 provider "databricks" {
-  alias         = "workspace"
-  host          = var.databricks_workspace_url
-  client_id     = var.databricks_client_id
-  client_secret = var.databricks_client_secret
+  alias     = "workspace"
+  auth_type = "github-oidc"
+  host      = var.databricks_workspace_url
+  client_id = var.databricks_client_id
+  # client_secret = var.databricks_client_secret
 }
