c04b345c87722d0cefead3539b3b593979314531
Pavlo Vovk <pvovk@origamirisk.com>
2025-07-29 17:10:53 -0500

    chore: cleanup

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "databricks_catalog" "sandbox" {
-  name           = "sandbox"
-  isolation_mode = "ISOLATED"
-  comment        = "This catalog is managed by Terraform"
+# Catalog had to be created manually in the Databricks UI before running this Terraform code.
+# Full privileges have been granted to the TERRAFORM_ADMIN service principal.
+data "databricks_catalog" "this" {
+  name = "sandbox"
+}
 
-  properties = {
-    purpose = "testing"
-  }
+output "catalog_info" {
+  value = data.databricks_catalog.this.catalog_info[0]
 }
