081523ed12a0f67279b76608cb6ff71c99e3ac56
Pavlo Vovk <pvovk@origamirisk.com>
2025-07-30 10:12:25 -0500

    fix: python stage

--- scripts/modify_job.py
+++ scripts/modify_job.py
@@ -1,19 +1,28 @@
+import os
 from databricks.sdk import WorkspaceClient
-from databricks.sdk.service import catalog
+from databricks.sdk.service import jobs
+
+JOB_NAME = os.getenv("JOB_NAME", "SQL Copy Job")
+CRON_EXPRESSION = os.getenv("CRON_EXPRESSION", "0 0 12 * * ?")
+TIMEZONE_ID = os.getenv("TIMEZONE_ID", "UTC")
 
 w = WorkspaceClient()
 
-all = w.catalogs.list()
-
-for c in all:
-    print(c.name)
-
-job_list = w.jobs.list(name="SQL Copy Job")
-
-job_id = job_list[0].job_id
-print(job_id)
-
-print(job_list)
-
+job_list = w.jobs.list(name=JOB_NAME)
 for j in job_list:
-    print(j)
+    job_id = j.job_id
+
+current_settings = w.jobs.get(job_id=job_id).settings
+print("Current job schedule:\n", str(current_settings.schedule.as_dict()))
+
+new_settings = jobs.JobSettings(
+    schedule=jobs.CronSchedule(
+        quartz_cron_expression=CRON_EXPRESSION, timezone_id=TIMEZONE_ID
+    )
+)
+print("New job schedule:\n", str(new_settings.schedule.as_dict()))
+
+try:
+    res = w.jobs.update(job_id=job_id, new_settings=new_settings)
+except Exception as e:
+    print("Oops!", e)
