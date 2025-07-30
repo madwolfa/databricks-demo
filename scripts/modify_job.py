062cc07d5187e831f689dfe2ee93a5d2198c5d32
Pavlo Vovk <pvovk@origamirisk.com>
2025-07-30 08:54:59 -0500

    fix: python stage

--- scripts/modify_job.py
+++ scripts/modify_job.py
@@ -1,19 +1,9 @@
 from os import getenv
 from databricks.sdk import WorkspaceClient
 
-databricks_host = getenv("DATABRICKS_HOST")
-databricks_client_id = getenv("DATABRICKS_CLIENT_ID")
-databricks_client_secret = getenv("DATABRICKS_CLIENT_SECRET")
-
-print(databricks_host)
-print(databricks_client_id)
-
-w = WorkspaceClient(
-  host          = databricks_host,
-  client_id     = databricks_client_id,
-  client_secret = databricks_client_secret
-)
+w = WorkspaceClient()
 
 run_list = w.jobs.list_runs()
 
-print(run_list)
+for j in run_list:
+    print(f"Run ID: {j.run_id}, State: {j.state}, Job ID: {j.job_id}")
