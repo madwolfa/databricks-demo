c24d9063dfc1433f630548329aab2796bccf6f01
Pavlo Vovk <pvovk@origamirisk.com>
2025-07-30 09:04:07 -0500

    fix: python stage

--- scripts/modify_job.py
+++ scripts/modify_job.py
@@ -1,8 +1,16 @@
 from databricks.sdk import WorkspaceClient
+from databricks.sdk.service import catalog
 
 w = WorkspaceClient()
 
+all = w.catalogs.list(catalog.ListCatalogsRequest())
+
+for c in all:
+    print(c.name)
+
 job_list = w.jobs.list()
 
+print(job_list)
+
 for j in job_list:
-    print(f"Job ID: {j.job_id}, State: {j.state}")
+    print(j)
