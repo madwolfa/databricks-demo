8362b4b9761ead9cd4b142ddf761d4cf9a008c7e
Pavlo Vovk <pvovk@origamirisk.com>
2025-07-30 15:34:21 -0500

    fix: python stage

--- scripts/modify_job.py
+++ scripts/modify_job.py
@@ -31,6 +31,13 @@ w = WorkspaceClient()
 
 # Helper function to update cron expression
 def update_cron(cron, **kwargs):
+    """Returns a cron expression with the specified parts updated.
+
+    Args:
+      cron (str): The cron expression to update.
+      **kwargs: Keyword arguments specifying the parts to update
+                (e.g., seconds, minutes, hours, dom, month, dow).
+    """
     parts = cron.split(" ")
     if len(parts) != 6:
         raise Exception(f"Invalid cron expression: {cron}")
@@ -48,39 +55,57 @@ def update_cron(cron, **kwargs):
     )
 
 
-job_list = w.jobs.list(name=JOB_NAME)
+def update_job(job_name="", weekdays_only=False):
+    """Returns True if the job schedule was updated successfully
 
-job_ids = []
-for j in job_list:
-    job_ids.append(j.job_id)
+    This function updates the schedule of a Databricks job to run on weekdays only or every day based on the value of the `weekdays_only` parameter.
+    If the job is not found or more than one is found - it raises an exception, otherwise it updates the schedule of the job and returns True.
 
-if len(job_ids) > 1:
-    print(f"Found more than one job with name '{JOB_NAME}': {job_ids}")
-    raise Exception(f"Found more than one job with name '{JOB_NAME}'.")
-elif len(job_ids) == 1:
-    job_id = job_ids[0]
-    print(f"Found job ID with name '{JOB_NAME}': {job_id}")
-else:
-    job_id = 0
+    Args:
+      job_name (str): The name of the job to update.
+      weekdays_only (bool): Whether to run the job on weekdays only (True) or every day (False).
+    """
+    if not job_name:
+        raise Exception("Please provide a job name!")
 
-if job_id:
-    current_schedule = w.jobs.get(job_id=job_id).settings.schedule
-    print(f"Current job schedule: {current_schedule.as_dict()}")
-    cron_expression = current_schedule.quartz_cron_expression
-    timezone_id = current_schedule.timezone_id
-    if WEEKDAYS_ONLY:
-        cron_expression = update_cron(cron_expression, dom="?", dow="MON-FRI")
+    job_list = w.jobs.list(name=job_name)
+
+    job_ids = []
+    for j in job_list:
+        job_ids.append(j.job_id)
+
+    if len(job_ids) > 1:
+        print(f"Found more than one job with name '{job_name}': {job_ids}")
+        raise Exception(f"Found more than one job with name '{job_name}'.")
+    elif len(job_ids) == 1:
+        job_id = job_ids[0]
+        print(f"Found job ID with name '{job_name}': {job_id}")
     else:
-        cron_expression = update_cron(cron_expression, dom="*", dow="?")
-    new_settings = jobs.JobSettings(
-        schedule=jobs.CronSchedule(
-            quartz_cron_expression=cron_expression,
-            timezone_id=timezone_id,
+        job_id = 0
+
+    if job_id:
+        current_schedule = w.jobs.get(job_id=job_id).settings.schedule
+        print(f"Current job schedule: {current_schedule.as_dict()}")
+        cron_expression = current_schedule.quartz_cron_expression
+        timezone_id = current_schedule.timezone_id
+        if weekdays_only:
+            cron_expression = update_cron(cron_expression, dom="?", dow="MON-FRI")
+        else:
+            cron_expression = update_cron(cron_expression, dom="*", dow="?")
+        new_settings = jobs.JobSettings(
+            schedule=jobs.CronSchedule(
+                quartz_cron_expression=cron_expression,
+                timezone_id=timezone_id,
+            )
         )
-    )
-    print(f"New job schedule: {new_settings.schedule.as_dict()}")
-    w.jobs.update(job_id=job_id, new_settings=new_settings)
-    print("Job schedule updated!")
-else:
-    print(f"Job with name '{JOB_NAME}' not found!")
-    raise Exception(f"Job with name '{JOB_NAME}' not found!")
+        print(f"New job schedule: {new_settings.schedule.as_dict()}")
+        w.jobs.update(job_id=job_id, new_settings=new_settings)
+        print("Job schedule updated!")
+        return True
+    else:
+        print(f"Job with name '{job_name}' not found!")
+        raise Exception(f"Job with name '{job_name}' not found!")
+
+
+if __name__ == "__main__":
+    update_job(job_name=JOB_NAME, weekdays_only=WEEKDAYS_ONLY)
