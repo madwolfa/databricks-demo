07ed3d8409276fe7ee0f777710ecf85458fcc580
Pavlo Vovk <pvovk@origamirisk.com>
2025-07-29 20:39:27 -0500

    chore: add stuff

--- main.tf
+++ main.tf
@@ -1,9 +1,91 @@
-# Catalog had to be created manually in the Databricks UI before running this Terraform code.
-# Full privileges have been granted to the TERRAFORM_ADMIN service principal.
+/* 
+   Prerequisites:
+     1. Catalog has been created manually in the Databricks UI with the default storage.
+     2. Service principal 'TERRAFORM_ADMIN' has been created with all privileges granted to it.
+*/
+
+# This Terraform code assumes that the catalog already exists. Let's look it up:
 data "databricks_catalog" "this" {
   name = "sandbox"
 }
 
+# Output the catalog information
 output "catalog_info" {
   value = data.databricks_catalog.this.catalog_info[0]
+}
+
+# Create a schema (database) in the existing catalog
+resource "databricks_schema" "this" {
+  catalog_name = data.databricks_catalog.this.id
+  name         = "nyctaxi"
+  comment      = "This database is managed by Terraform"
+  properties = {
+    catalog = data.databricks_catalog.this.name
+    owner   = "TERRAFORM_ADMIN"
+  }
+}
+
+# Let's lookup our serverless SQL warehouse
+data "databricks_sql_warehouse" "this" {
+  name = "Serverless Starter Warehouse"
+}
+
+# Lookup the source table to extract its schema
+data "databricks_table" "source" {
+  name = "samples.nyctaxi.trips"
+}
+
+# Create a managed table in the schema
+resource "databricks_sql_table" "this" {
+  name         = "trips"
+  catalog_name = data.databricks_catalog.this.name
+  schema_name  = databricks_schema.this.name
+  table_type   = "MANAGED"
+  warehouse_id = data.databricks_sql_warehouse.this.id
+
+  # Replicate the source table schema
+  dynamic "column" {
+    for_each = data.databricks_table.source.table_info[0].columns
+    content {
+      name = column.value.name
+      type = column.value.type_text
+    }
+  }
+
+  comment = "This table is managed by Terraform"
+}
+
+resource "databricks_query" "copy_query" {
+  warehouse_id = data.databricks_sql_warehouse.this.id
+  query_text   = <<EOT
+MERGE WITH SCHEMA EVOLUTION INTO
+  ${databricks_sql_table.this.id} AS target
+USING
+  ${data.databricks_table.source.name} AS source
+ON
+  source.tpep_pickup_datetime = target.tpep_pickup_datetime
+  AND source.tpep_dropoff_datetime = target.tpep_dropoff_datetime
+WHEN MATCHED THEN UPDATE SET *
+WHEN NOT MATCHED THEN INSERT *
+WHEN NOT MATCHED BY SOURCE THEN DELETE
+EOT
+  display_name = "Copy Query"
+}
+
+resource "databricks_job" "sql_copy_job" {
+  name        = "SQL Copy Job"
+  description = "This job copies data from the source table to the managed table using a SQL query."
+  task {
+    task_key = "run_copy_query"
+    sql_task {
+      warehouse_id = data.databricks_sql_warehouse.this.id
+      query {
+        query_id = databricks_query.copy_query.id
+      }
+    }
+  }
+  schedule {
+    quartz_cron_expression = "0 0 12 * * ?" # Runs daily at noon
+    timezone_id            = "UTC"
+  }
 }
